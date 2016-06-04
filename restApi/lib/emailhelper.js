b1832a628fb2cd34bc28dcc52a9c8de84b17a00c
Saikiran Daripelli <sdaripelli@Saikirans-Mac.local>
2016-06-04 19:09:21 +0530

    Email and SMS

---
+++ restApi/lib/emailhelper.js
@@ -0,0 +1,57 @@
+var nodemailer = require("nodemailer");
+var sgTransport = require("nodemailer-sendgrid-transport");
+var jwt         = require("jsonwebtoken");
+var util        = require("util");
+var path        = require("path");
+var options = {
+    auth: {
+        api_key: process.env.sendgridKey 
+    }
+}
+var EmailTemplate = require("email-templates").EmailTemplate;
+var transporter = nodemailer.createTransport(sgTransport(options));
+var templateDir = path.join(__dirname, "./", "templates");
+console.info("email templates Dir ", templateDir);
+var sendJoinEmail = function (email,familyId, institutionId, role) {
+    return new Promise(function(resolve,reject){
+        var registerEmail = {
+            "email":email,
+            "familyId":familyId,
+            "institutionId":institutionId,
+            "role":role
+        };
+        var token = jwt.sign(registerEmail, process.env.jwtSecret,{ expiresIn: 60*60*48 });
+        var registrationLink=util.format("%s/register?token=%s", process.env.uihost, token);
+        console.debug("registrationLink Link " + registrationLink);
+        var registrationEmailTemplate = new EmailTemplate(templateDir + "/registration");
+        registrationEmailTemplate.render({
+            registrationLink: registrationLink
+        },function (err, result) {
+            console.info(result);
+            
+        });
+        var registrationEmail = transporter.templateSender(registrationEmailTemplate , {
+            from: "no-reply@secureslice.com"
+        });
+        verificationEmail({
+            to: email,
+            subject: "Parent Registration Invitation"
+        }, {
+            registrationLink: registrationLink,
+            email: email
+        }, function(err, info){
+            if (err){
+                console.error("Error sending email ", err);
+                reject(err);
+            }
+            else {
+                console.debug("Message sent: ", info);
+                resolve(info);
+            }
+        });
+    });
+}
+
+module.exports = {
+    sendJoinEmail
+}
