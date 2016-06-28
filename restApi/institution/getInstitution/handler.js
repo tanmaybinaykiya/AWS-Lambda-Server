c661b514840a7bd3c23f09d37902c2bd1134e467
Saikiran Daripelli <sdaripelli@Saikirans-Mac.local>
2016-06-29 00:36:37 +0530

    Fix getInstitution

--- restApi/institution/getInstitution/handler.js
+++ restApi/institution/getInstitution/handler.js
@@ -1,7 +1,9 @@
 var institutionLib = require("../../lib/institution");
 var handleRequest = require("../../lib/handler");
+var HttpError = require("../../lib/errors").HttpError;
 module.exports.handler = function (event, context, cb) {
   handleRequest(cb, function* (ctx) {
+    var institutionShortCode= event.queryParams.shortCode;
     if (institutionShortCode.indexOf(institutionLib.restrictedCodes) >= 0) {
         ctx.body=  {
             name: "secureslice",
@@ -9,13 +11,14 @@ module.exports.handler = function (event, context, cb) {
         }
         return;
     }
-    var subDomain=event.queryParams.shortCode;
-    if(process.env.SERVERLESS_STAGE==="beta"){
-      subDomain=subDomain.split("-beta")[0];
+    var institution = yield institutionLib.getInstitution(institutionShortCode);
+    if(institution){
+      ctx.body=institution.toJSON();
+      ctx.body.type="institution";
+      return;
+    }else{
+      throw new HttpError(404, "institution shortcode is not valid");
     }
-    var institution = yield institutionLib.getInstitution(subDomain);
-    ctx.body=institution.toJSON();
-    ctx.body.type="institution";
-    return;
+    
   });
 };
